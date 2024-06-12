1f61b96878a17a0f66c1cf2b02e452087bea3d47
Roja J <96616787+21wh1a6614@users.noreply.github.com>
2024-06-12 10:11:45 +0530

    Add files via upload

---
+++ arrlist.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class arrlist{
+public static void main(String args[]){
+ArrayList <String> c = new ArrayList<String>();
+c.add("A");
+c.add("B");
+c.add("C");
+System.out.println("Original array list:" + c);
+System.out.println("\nPrint using index f an element:");
+int n = c.size();
+for(int i =0;i<n;i++)
+        System.out.println(c.get(i));
+        }
+        }
